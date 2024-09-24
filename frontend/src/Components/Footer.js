046447cba3dedf8c28a9489fd1e764f14f5454be
Harshitgupta5290 <harshitgupta5290@gmail.com>
2024-09-24 22:57:49 +0530

    fix

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -15,8 +15,8 @@ function Footer() {
                         <h1>Contact Us</h1>
                         <p>Librarian</p>
                         <p>Government School</p>
-                        <p>Visakhapatnam-530041</p>
-                        <p>Andhra Pradesh</p>
+                        <p>Cannaught Place-110051</p>
+                        <p>New Delhi</p>
                         <p>India</p>
                         <p><b>Email:</b>example@gmail.com</p>
                     </div>
@@ -31,7 +31,7 @@ function Footer() {
                         <h1>Librarian</h1>
                         <p>Name</p>
                         <p>Education</p>
-                        <p>Contact: +91 9123456787</p>
+                        
                     </div>
                 </div>
                 <div className="contact-social" >
@@ -42,7 +42,7 @@ function Footer() {
                 </div>
             </div>
             <div className='copyright-details'>
-                <p className='footer-copyright'>&#169; 2020 copyright all right reserved<br /><span>Designed with ❤️ by Pranavdhar Reddy</span></p>
+                <p className='footer-copyright'>&#169; 2024 copyright all right reserved<br /><span></span></p>
             </div>
         </div>
     )
